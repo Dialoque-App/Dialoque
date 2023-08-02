060905c4012b4347d35f1df0930b1e5a4ea9e76c
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-08-02 16:51:10 +0700

    create game center

--- Dialoque/Dialoque/ContentView.swift
+++ Dialoque/Dialoque/ContentView.swift
@@ -10,43 +10,92 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
+    @EnvironmentObject var statController: StatController
+    @EnvironmentObject var gameKitController: GameKitController
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    
+    @AppStorage("score") var score: Int = 0
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack{
+                List {
+                    ForEach(items) { item in
+                        NavigationLink {
+                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        } label: {
+                            Text(item.timestamp!, formatter: itemFormatter)
+                        }
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        EditButton()
+                    }
+                    ToolbarItem {
+                        Button(action: addItem) {
+                            Label("Add Item", systemImage: "plus")
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                VStack{
+                    HStack{
+                        Text("Score:")
+                        Button(action: {
+                            if self.score <= 0{
+                                self.score = 0
+                            } else {
+                                self.score -= 1
+                            }
+                        }){
+                            Text("-").bold()
+                        }
+                        Text("\(score)")
+                        Button(action: {
+                            self.score += 1
+                        }){
+                            Text("+").bold()
+                        }
                     }
+                    
+                    Spacer().frame(height: 16)
+                    
+                    Button(action: {
+                        gameKitController.reportScore(totalScore: self.score)
+                    }){
+                        Text("Submit")
+                            .bold()
+                            .foregroundColor(.black)
+                    }
+                    
+                    Spacer().frame(height: 8)
+                    
+                    Button(action: {
+                        let newView = GameCenterView().ignoresSafeArea()
+                        UIApplication.shared.windows.first?.rootViewController = UIHostingController(rootView: newView)
+                    }){
+                        Text("Leaderboard")
+                            .bold()
+                            .foregroundColor(.indigo)
+                    }
+                    
                 }
+                
             }
-            Text("Select an item")
         }
     }
-
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -57,11 +106,11 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -83,6 +132,10 @@ private let itemFormatter: DateFormatter = {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        @StateObject var gameKitController = GameKitController()
+        
+        ContentView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+            .environmentObject(gameKitController)
     }
 }
--- Dialoque/Dialoque/DialoqueApp.swift
+++ Dialoque/Dialoque/DialoqueApp.swift
@@ -10,11 +10,14 @@ import SwiftUI
 @main
 struct DialoqueApp: App {
     let persistenceController = PersistenceController.shared
+    
+    @StateObject var gameKitController = GameKitController()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(gameKitController)
         }
     }
 }
